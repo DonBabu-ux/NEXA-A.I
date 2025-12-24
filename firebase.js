a3dbc1de78a5228d2fb6e4bd66308f43d61024e0
Don the Techie <lewismwangi990@gmail.com>
2025-12-24 23:13:39 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,17 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyAkMFYizmJ4dd8G8oJ9uw1JdgzwhtmsENU",
+  authDomain: "mywebapp-bb780.firebaseapp.com",
+  projectId: "mywebapp-bb780",
+  storageBucket: "mywebapp-bb780.firebasestorage.app",
+  messagingSenderId: "1020137155241",
+  appId: "1:1020137155241:web:82f69c3f184c3fded897b8",
+  measurementId: "G-VQQ5D91E38"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export Auth
 export const auth = getAuth(app);
